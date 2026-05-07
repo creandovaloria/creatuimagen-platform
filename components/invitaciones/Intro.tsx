467992bf0f83bac234086682090b93cd5e3d4922
creandovaloria <creandovalor.ia@gmail.com>
2026-05-07 02:10:41 -0600

    📱 Fit intro+player on first screen

--- components/invitaciones/Intro.tsx
+++ components/invitaciones/Intro.tsx
@@ -1,8 +1,14 @@
 interface Props { src: string; }
+
 export default function Intro({ src }: Props) {
   return (
-    <section className="w-full leading-none">
-      <img src={src} alt="Invitación XV años Regina" className="w-full h-auto block" />
+    <section className="w-full leading-none" style={{ maxHeight: "55vh", overflow: "hidden" }}>
+      <img
+        src={src}
+        alt="Invitación XV años Regina"
+        className="w-full block object-cover object-top"
+        style={{ height: "55vh" }}
+      />
     </section>
   );
 }
