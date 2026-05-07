7d1f3a44406685999ee2445bcfd96249026d3eec
creandovaloria <creandovalor.ia@gmail.com>
2026-05-06 18:53:42 -0600

    Add components/invitaciones/Intro.tsx

---
+++ components/invitaciones/Intro.tsx
@@ -0,0 +1,8 @@
+interface Props { src: string; }
+export default function Intro({ src }: Props) {
+  return (
+    <section className="w-full leading-none">
+      <img src={src} alt="Invitación XV años Regina" className="w-full h-auto block" />
+    </section>
+  );
+}
